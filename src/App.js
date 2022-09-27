70706af937f0fc8db0f39705440ac41c59892ea2
mike macfarland <m_macfarland@live.com>
2022-09-27 19:36:45 -0400

    initial commit with router and wireframe

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./pages/Home";
+import About from "./pages/About";
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">container
+      <nav className="nav">nav</nav>
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home/>} />
+          <Route exact path='/about' element={<About/>} />
+        </Routes>
+      </Router>
+
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className="content">
+        <div className="search">search</div>
+        <div className="content">
+        content
+        </div>
+    </div>
+  )
+}
+
+export default Home
+
