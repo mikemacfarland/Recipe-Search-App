c987498b3d1bfeb184b30885fbb1edd0863dae9d
mike macfarland <m_macfarland@live.com>
2022-10-03 13:05:37 -0400

    restyled nav for mobile, added login and favorite page

--- src/components/LogoLink.jsx
+++ src/components/LogoLink.jsx
@@ -1,12 +1,13 @@
+import { Link } from 'react-router-dom'
 import {ReactComponent as SpoonFork} from '../assets/icons/spoonfork.svg'
 
 function LogoLink() {
   return (
-    <div>
+    <Link className='logo' to='/'>
         <p className='logo__text'>THE</p>
         <SpoonFork className='logo__img'/>
         <p className='logo__text'>SAUCE</p>
-    </div>
+    </Link>
   )
 }
 
